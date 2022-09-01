d75c2ade2fd1774d26a29d9d9b1b41fdcb5f82c3
geometrika-web <geometrika.web@gmail.com>
2022-09-01 14:59:30 +0200

    back button

--- js/main.js
+++ js/main.js
@@ -35,6 +35,50 @@ if (window.location.hash !== '') {
         center = [parseFloat(parts[1]), parseFloat(parts[2])];
     }
 }
+
+// NO BACK FUNCTION
+/* (function (global) {
+    
+    if (typeof global === 'undefined') {
+        throw new Error('window is undefined');
+    }
+
+    var _hash = '!';
+    var noBackPlease = function () {
+        global.location.href += '#';
+
+        // Making sure we have the fruit available for juice (^__^)
+        global.setTimeout(function () {
+            global.location.href += '!';
+        }, 50);
+    };
+
+    global.onhashchange = function () {
+        if (global.location.hash !== _hash) {
+            global.location.hash = _hash;
+        }
+    };
+
+    global.onload = function () {
+        noBackPlease();
+
+        // Disables backspace on page except on input fields and textarea..
+        document.body.onkeydown = function (e) {
+            var elm = e.target.nodeName.toLowerCase();
+            if (e.which === 8 && elm !== 'input' && elm !== 'textarea') {
+                e.preventDefault();
+            }
+            // Stopping the event bubbling up the DOM tree...
+            e.stopPropagation();
+        };
+    };
+})(window); */
+
+window.onpopstate = function (e) {
+    e.preventDefault();
+    window.open('index.html', '_self');
+    // toggle(document.getElementById('menu-toggle'));
+};
 //var imageExtent = [1917834.45, 5421643.41, 2500000, 6500000];
 //var imageExtent2 = [2500000, 5421000, 2500000, 6500000];
 
