417f4fba2506a4da67ef54b155bc47092029267c
tlev <47306060+tlev22@users.noreply.github.com>
2023-07-01 16:32:17 +0200

    Commit nakon backenda

--- js/config.js
+++ js/config.js
@@ -1,9 +1,9 @@
 // uppercase for constants
-export const API_URL = 'http://127.0.0.1:8000/api/v1';
-export const APP_URL = 'http://127.0.0.1:8000/';
-/* export const API_URL =
+/* export const API_URL = 'http://127.0.0.1:8000/api/v1';
+export const APP_URL = 'http://127.0.0.1:8000/'; */
+export const API_URL =
     'https://tomislav-guides-0f0bcc422a02.herokuapp.com/api/v1';
-export const APP_URL = 'https://tomislav-guides-0f0bcc422a02.herokuapp.com/'; */
+export const APP_URL = 'https://tomislav-guides-0f0bcc422a02.herokuapp.com/';
 export const STORAGE_URL =
     'https://tomislav-guides.s3.eu-central-1.amazonaws.com';
 export const KEY = '0eed218f-70bf-4412-b80f-fbeccbc5f106';
