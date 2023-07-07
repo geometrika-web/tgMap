9800fd281c2e9029b490d9f1d4ec1292bb9f5f76
tlev <47306060+tlev22@users.noreply.github.com>
2023-07-07 12:02:32 +0200

    popravljeni atributi u funkciji navigator.share()

--- js/views/eventView.js
+++ js/views/eventView.js
@@ -31,20 +31,20 @@ class EventView extends View {
     }
 
     async shareEvent() {
-        const blob = await fetch(this._data.images[0]).then((img) =>
+        /* const blob = await fetch(this._data.images[0]).then((img) =>
             img.blob()
-        );
+        ); */
         await navigator.share({
             title: this._data[`name${localStorage.lang}`],
             text: this._data[`description${localStorage.lang}`],
-            url: window.location.href,
-            files: [
+            url: window.location.href
+            /* files: [
                 new File([blob]),
                 `this._data[name${localStorage.lang}].jpg`,
                 {
                     type: 'image/jpg'
                 }
-            ]
+            ] */
         });
     }
 
